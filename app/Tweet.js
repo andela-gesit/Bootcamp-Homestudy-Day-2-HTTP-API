b703a9e7531fbc0cb56d4f88b05ff0bc832f8253
idem <victoridongesit@gmail.com>
2017-02-14 16:58:38 +0100

    Post to Me Removed

--- app/Tweet.js
+++ app/Tweet.js
@@ -7,15 +7,6 @@ var client = new Twitter({
   access_token_key: '91315704-ubhlY8BrLIatC5SvQAYj3axQBwQDr6vlxlTs7zlvS',
   access_token_secret: 'BWSIhn2FTVRN77aA9ef4uBisk5kIJzQkbfCJp15IQwaFg'
 });
-/* 
-client.post('statuses/update', {status: 'Tweeted!'},  function(error, tweet, response){
-  if(error){
-    console.log('An error occured');
-  }
-  console.log(tweet);  // Tweet body.
-  console.log(response);  // Raw response object.
-});
-*/
 var params = {screen_name: 'Andela'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
