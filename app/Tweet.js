c80e07761145afa8a9d648a4a4445ef9df0d5c2b
idem <victoridongesit@gmail.com>
2017-02-14 16:57:29 +0100

    Post to My Timeline

--- app/Tweet.js
+++ app/Tweet.js
@@ -7,15 +7,15 @@ var client = new Twitter({
   access_token_key: '91315704-ubhlY8BrLIatC5SvQAYj3axQBwQDr6vlxlTs7zlvS',
   access_token_secret: 'BWSIhn2FTVRN77aA9ef4uBisk5kIJzQkbfCJp15IQwaFg'
 });
-/* 
+
 client.post('statuses/update', {status: 'Tweeted!'},  function(error, tweet, response){
   if(error){
     console.log('An error occured');
   }
-  console.log(tweet);  // Tweet body.
-  console.log(response);  // Raw response object.
+  console.log(tweet);  
+  console.log(response);  
 });
-*/
+
 var params = {screen_name: 'Andela'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
@@ -24,7 +24,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 client.stream('statuses/filter', {track: '#Andela'}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
+    console.log(tweet);
   });
 
   stream.on('error', function(error) {
